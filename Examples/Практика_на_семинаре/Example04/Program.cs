48d415a8aaca5a2c5455726d8cc42a3ad76d65c2
Olga Serbul <Olsen.zika@mail.ru>
2022-11-15 17:47:18 +0300

    практика_на_семинаре_example04

---
+++ Examples/Практика_на_семинаре/Example04/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте одномерный массив из 123 случайных чисел.
+// Найдите количество элементов массива, значения которых
+// лежат в отрезке [10;99].
+
+int[] array = new int[123];
+int count = 0;
+for(int i = 0; i < array.Length; i++ )
+{
+    array[i] = new Random().Next(-100, 1000);
+    Console.Write(array[i] + "  ");
+}
+for(int i = 0; i < array.Length; i++ )
+{
+    if(array[i] > 11 && array[i] < 100)
+    {
+        count++;
+
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Количество элементов = {count}");
