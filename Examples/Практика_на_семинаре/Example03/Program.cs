e4558ca15c124208e2e9a115d670345c89da30c1
Olga Serbul <Olsen.zika@mail.ru>
2022-11-15 16:38:46 +0300

    практика_на_семинаре_example03

---
+++ Examples/Практика_на_семинаре/Example03/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задайте массив. Напишите программу, которая
+// определяет, присутсвует ли заданное число в массиве.
+int[] array = new int[12] {1, 6, 78, 3, -5, -13, 45, 0, 21, 16, 20, 42 };
+Console.Write("Введите искомое число:  ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+bool SearchElement(int[] array, int X )
+{
+    for ( int i = 0; i < array.Length; i++ )
+    {
+        if(array[i] == X)
+        {
+            return true;
+        }
+    }
+    return false;
+
+}
+
+Console.WriteLine(SearchElement(array, X ));
