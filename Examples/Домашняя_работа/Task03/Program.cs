f5576dbd82a9c422dabab6cf3b92835d91b86292
Olga Serbul <Olsen.zika@mail.ru>
2022-11-16 23:17:48 +0300

    домашняя_работа_task03

---
+++ Examples/Домашняя_работа/Task03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте массив вещественных чисел. Найдите разницу 
+// между максимальным и минимальным элементом массива.
+double[] array = new double[7] {1.5 , 3.6 , 68.4 , 34.9 , 24.4 , 124.5 , 42.1 };
+for(int i = 0; i < array.Length; i++ )
+{
+    Console.Write(array[i] + " ");
+}
+Console.WriteLine();
+double DiffMinMax (double[] array )
+{ 
+    double max = array[0];
+    double min = array[0];
+    double MinMax = 0;
+    for(int i = 0; i < array.Length ; i++ )
+    {
+        if(min > array[i])
+        {
+            min = array[i];
+        }
+        if(max < array[i])
+        {
+            max = array[i];
+        }
+    }  
+    Console.WriteLine("min = " + min);
+    Console.WriteLine("max = " + max);
+    MinMax = max - min;
+    return MinMax;
+}
+Console.WriteLine($"Разница между min и max элементом = {DiffMinMax(array)}");
