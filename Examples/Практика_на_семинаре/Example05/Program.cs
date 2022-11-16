021739c0757466eb3c66cc669368d3ebfaf4d4cb
Olga Serbul <Olsen.zika@mail.ru>
2022-11-16 18:05:30 +0300

    практика_на_семинаре_example05

---
+++ Examples/Практика_на_семинаре/Example05/Program.cs
@@ -0,0 +1,28 @@
+﻿// Найдите произведение пар чисел в одномерном массиве.
+// пары - первый и последний, второй и предпоследний и т.д.
+// Результат запишите в новом массиве.
+ 
+
+Console.Write("Сколько элементов будет в массиве? Ведите число:  ");
+int Length = Convert.ToInt32(Console.ReadLine());
+ 
+int[] array = new int[Length]; 
+
+for (int i = 0; i < Length; i++ )
+{
+  Console.Write($"Введите элемент массива под номером  {i}: ");
+   array[i] = Convert.ToInt32(Console.ReadLine());
+}
+
+ int number = 0; 
+if(Length % 2 == 0)
+{
+
+    for( int i = 0; i < Length / 2; i++)
+    {
+       
+       number = array[i] * array[array.Length - i - 1];
+       
+    }
+    Console.Write($"Новый массив: {number} " + "  ");
+}
