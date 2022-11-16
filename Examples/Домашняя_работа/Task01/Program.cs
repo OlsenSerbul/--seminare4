2b1ecfba385fc685b08fb82b886becfade174623
Olga Serbul <Olsen.zika@mail.ru>
2022-11-16 19:27:06 +0300

    домашняя_работа_task01_метод

--- Examples/Домашняя_работа/Task01/Program.cs
+++ Examples/Домашняя_работа/Task01/Program.cs
@@ -1,24 +1,26 @@
 ﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
 //  Напишите программу, которая покажет количество чётных чисел в массиве.
-
 Console.Write("Сколько элементов будет в массиве? Ведите число:  ");
 int Length = Convert.ToInt32(Console.ReadLine());
  
 int[] array = new int[Length];
-
 for(int i = 0; i < Length; i++)
 {
     array[i] = new Random().Next(100, 1000);
     Console.Write(array[i] + " ");
 }
-Console.WriteLine();
-
-int count = 0;
-for(int i = 0; i < Length; i++ )
+int CountChet (int[] array, int Length)
 {
-    if (array[i] % 2 == 0)
-    {
+   int count = 0;
+   for(int i = 0; i < Length; i++ )
+   {
+      if (array[i] % 2 == 0)
+      {
         count++;
-    }
-}
+      }
+   }
+   return count;
+}   
+int count = CountChet(array, Length);
+Console.WriteLine();
 Console.WriteLine($"Количество четных чисел в массиве = {count}");
---
+++ Examples/Домашняя_работа/Task02/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+Console.Write("Сколько элементов будет в массиве? Ведите число:  ");
+int Length = Convert.ToInt32(Console.ReadLine());
+ 
+int[] array = new int[Length];
+
+for(int i = 0; i < Length; i++)
+{
+    array[i] = new Random().Next(0, 100);
+    Console.Write(array[i] + " ");
+}
+int sumNech = 0;
+for(int i = 0 ; i < Length; i++ )
+{
+    if( i % 2 > 0)
+    {
+        sumNech += array[i];
+    }
+}
+Console.WriteLine();
+Console.WriteLine($"Сумма элементов на нечетных позициях  = {sumNech}");
+
